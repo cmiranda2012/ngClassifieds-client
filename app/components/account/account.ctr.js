f213cdacd3858df40be83ce7317394c4592e7b56
Carolina Miranda <camrfn1@gmail.com>
2017-03-22 19:01:16 -0400

    Add account controller and template

---
+++ app/components/account/account.ctr.js
@@ -0,0 +1,90 @@
+(function() {
+
+    "use strict";
+
+    angular
+        .module("ngClassifieds")
+        .controller('accountCtrl', function($http, $scope, $mdToast, $mdSidenav, $mdDialog, $state, classifiedsFactory, AuthService) {
+
+            const vm = this;
+
+            vm.user;
+            vm.classifieds;
+            vm.deleteClassified = deleteClassified;
+            vm.editClassified = editClassified;
+            vm.logout = logout;
+            vm.home = home;
+
+            classifiedsFactory.getUser().then(function(user) {
+                vm.user = user.data;
+            });
+
+            classifiedsFactory.getUserClassifieds().then(function(classifieds) {
+                vm.classifieds = classifieds.data;
+            });
+
+            $scope.$on('editSaved', function(event, message, classified) {
+                $http.put(`http://localhost:8081/api/classifieds/${classified._id}`, classified)
+                    .success(function(data) {
+                        showToast('Classified saved');
+                    })
+                    .error(function(data) {
+                        console.log(`Error: ${data}`);
+                    });
+            });
+
+            function editClassified(classified) {
+                $state.go('account.edit', {
+                    classified: classified,
+                    id: classified.id
+                });
+            }
+
+            function home() {
+                $state.go('classifieds');
+            }
+
+            function deleteClassified(event, classified, id) {
+
+                var confirm = $mdDialog.confirm()
+                    .title(`Are you sure you want to delete ${classified.title}?`)
+                    .ok('Yes')
+                    .cancel('No')
+                    .targetEvent(event);
+                $mdDialog.show(confirm).then(function() {
+                    $http.delete(`http://localhost:8081/api/classifieds/${id}`)
+                        .success(function(data) {
+                            vm.classifieds = data;
+                            showToast('Classified Deleted');
+                        })
+                        .error(function(data) {
+                            showToast('Unable to delete');
+                            console.log(`Error: ${data}`);
+                        });
+
+                }, function() {
+
+                });
+            }
+
+            function logout() {
+                AuthService.logout()
+                    .then(function() {
+                        $state.go('classifieds', null, {
+                            reload: true
+                        });
+                    });
+            }
+
+            function showToast(message) {
+                $mdToast.show(
+                    $mdToast.simple()
+                    .content(message)
+                    .position('top, right')
+                    .hideDelay(3000)
+                );
+            }
+
+        });
+
+})();
